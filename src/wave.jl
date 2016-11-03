4e2a4c37f2c2495c3d324bf64d9f722a90b356dc
Antony Schutz <antonyschutz@gmail.com>
2016-11-03 14:10:34 +0100

    faster a_trous

--- src/wave.jl
+++ src/wave.jl
@@ -4,8 +4,8 @@ function iuwt_decomp(x::Array{Float64,2}, scale::Int64; store_c0=false)
     c0 = copy(x)
 
     for i in 1:scale
-        c = for_a_trous(c0,i)
-        c1 = for_a_trous(c,i)
+        c = a_trous(c0,i)
+        c1 = a_trous(c,i)
         coeff[:,:,i] = c0 - c1
         c0 = copy(c)
     end
@@ -27,54 +27,19 @@ function iuwt_recomp(x::Array{Float64,3}, scale::Int64; c0=false)
     end
 
     for i in range(max_scale,-1,size(x,3))
-        recomp = for_a_trous(recomp,i) + x[:,:,i-scale]
+        recomp = a_trous(recomp,i) + x[:,:,i-scale]
     end
     if scale > 0
         for i in range(scale,-1,scale)
-            recomp = for_a_trous(recomp,i)
+            recomp = a_trous(recomp,i)
         end
     end
 
     return recomp
 end
 
+
 function a_trous(c0::Array{Float64,2}, scale::Int64)
-
-    filter = (1/16)*[1,4,6,4,1]
-    scale = scale - 1
-    tmp = filter[3]*c0
-
-    tmp[(2^(scale+1))+1:end,:] += filter[1]*c0[1:(end-(2^(scale+1))),:]
-    tmp[1:(2^(scale+1)),:] += filter[1]*c0[2^(scale+1):-1:1,:]
-
-    tmp[(2^(scale))+1:end,:] += filter[2]*c0[1:(end-(2^(scale))),:]
-    tmp[1:(2^(scale)),:] += filter[2]*c0[2^(scale):-1:1,:]
-
-    tmp[1:(end-(2^(scale))),:] += filter[4]*c0[(2^(scale))+1:end,:]
-    tmp[(end-(2^(scale)))+1:end,:] += filter[4]*c0[end:-1:end-2^(scale)+1,:]
-
-    tmp[1:(end-(2^(scale+1))),:] += filter[5]*c0[2^(scale+1)+1:end,:]
-    tmp[(end-(2^(scale+1))+1):end,:] += filter[5]*c0[end:-1:end-2^(scale+1)+1,:]
-
-    c1 = filter[3]*tmp
-
-    c1[:,(2^(scale+1))+1:end] += filter[1]*tmp[:,1:(end-(2^(scale+1)))]
-    c1[:,1:(2^(scale+1))] += filter[1]*tmp[:,2^(scale+1):-1:1]
-
-    c1[:,(2^(scale))+1:end] += filter[2]*tmp[:,1:(end-(2^(scale)))]
-    c1[:,1:(2^(scale))] += filter[2]*tmp[:,2^(scale):-1:1]
-
-    c1[:,1:(end-(2^(scale)))] += filter[4]*tmp[:,(2^(scale))+1:end]
-    c1[:,(end-(2^(scale))+1):end] += filter[4]*tmp[:,end:-1:end-2^(scale)+1]
-
-    c1[:,1:(end-(2^(scale+1)))] += filter[5]*tmp[:,(2^(scale+1))+1:end]
-    c1[:,(end-(2^(scale+1))+1):end] += filter[5]*tmp[:,end:-1:end-2^(scale+1)+1]
-
-    return c1
-
-end
-
-function for_a_trous(c0::Array{Float64,2}, scale::Int64)
 
   filter = (1/16)*[1,4,6,4,1]
 
@@ -114,7 +79,7 @@ function for_a_trous(c0::Array{Float64,2}, scale::Int64)
   end
 
   c1 = filter[3]*tmp
-  
+
   for k in 1:N
   for ind1N in ind1+1:N
       c1[k,ind1N] += filter[1]*tmp[k,ind1N - ind1]
